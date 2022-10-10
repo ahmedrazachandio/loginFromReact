2219c298cefc7e6cc18b971aa3e024b3f08860ea
zaidasim2197 <https://github.com/zaidasim2197/github-link-in-cloud.git>
2022-10-10 22:30:02 +0500

    code push

---
+++ src/components/login/login.jsx
@@ -0,0 +1,18 @@
+import './login.css';
+
+
+
+function Login() {
+    return (
+      <div className="login">
+        <div className='imgBox'>
+        </div>
+        <div className='loginBox'>
+            <span className='textLogin'>Login to continue</span>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
